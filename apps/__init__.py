34a6b2c68b6e7b90aa5fed2e9bdcc85ee95dba38
zhonghao <zh826256645@gmail.com>
2017-03-20 23:37:19 +0800

    初始化版本库

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding=utf-8 -*-
+"""
+初始化 app
+"""
+from flask import Flask
+
+
+def create_app(cfg):
+    """
+    初始化系统
+    """
+
+    # 初始化 Flask app
+    app = Flask(__name__)
+
+    # 加载配置文件
+    app.config.from_pyfile(cfg)
+    return app
